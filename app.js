988691e354aec62d6a31d1437ac6da65f1db3327
Raj Thombare (Rakaa) <therajthombare@gmail.com>
2021-06-27 00:27:09 +0530

    input processing output done

---
+++ app.js
@@ -0,0 +1,30 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+};
+
+function errorHandler(error){
+    console.log("Error Occurred!", error)
+    console.log("something is wrong with server! please try again later")
+}
+
+
+function clickHandler(){
+
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
